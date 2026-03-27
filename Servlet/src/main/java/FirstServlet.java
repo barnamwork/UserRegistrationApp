716642e23f8fe3bbb8ce3e9188b674e49eb5ffd1
Barnamwork <barnamwork@gmail.com>
2026-03-27 19:19:07 +0530

    [Barnam] UC1 Add FirstServlet implementation

---
+++ Servlet/src/main/java/FirstServlet.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/first")
+public class FirstServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello I Am Barnam</h1>");
+        out.println("<p>UC1 First Servlet Working</p>");
+    }
+}
